48f2126135be63fbead58653fb7059904aad8e8e
kaho <kaho0769@qq.com>
2023-06-25 18:17:49 +0800

    feat: pooled complex array storage

---
+++ src/Qynit.Pulsewave/ComplexArraySpan.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace Qynit.Pulsewave;
+internal readonly ref struct ComplexArraySpan<T>
+    where T : struct
+{
+    public Span<T> DataI { get; }
+    public Span<T> DataQ { get; }
+    public int Length => DataI.Length;
+    internal ComplexArraySpan(Span<T> dataI, Span<T> dataQ)
+    {
+        Debug.Assert(dataI.Length == dataQ.Length);
+        DataI = dataI;
+        DataQ = dataQ;
+    }
+    public static implicit operator ComplexArraySpan<T>(PooledComplexArray<T> source)
+    {
+        return new ComplexArraySpan<T>(source.DataI, source.DataQ);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public ComplexArraySpan<T> Slice(int start)
+    {
+        return new ComplexArraySpan<T>(DataI[start..], DataQ[start..]);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public ComplexArraySpan<T> Slice(int start, int length)
+    {
+        return new ComplexArraySpan<T>(DataI.Slice(start, length), DataQ.Slice(start, length));
+    }
+}
---
+++ src/Qynit.Pulsewave/PooledComplexArray.cs
@@ -0,0 +1,75 @@
+﻿using System.Buffers;
+
+using CommunityToolkit.Diagnostics;
+
+namespace Qynit.Pulsewave;
+internal sealed class PooledComplexArray<T> : IDisposable
+    where T : struct
+{
+    private readonly T[] _dataI;
+    private readonly T[] _dataQ;
+    private bool _disposed;
+    public int Length { get; }
+    public Span<T> DataI
+    {
+        get
+        {
+            if (_disposed)
+            {
+                ThrowHelper.ThrowObjectDisposedException(nameof(Waveform));
+            }
+            return _dataI.AsSpan(0, Length);
+        }
+    }
+    public Span<T> DataQ
+    {
+        get
+        {
+            if (_disposed)
+            {
+                ThrowHelper.ThrowObjectDisposedException(nameof(Waveform));
+            }
+            return _dataQ.AsSpan(0, Length);
+        }
+    }
+
+    public PooledComplexArray(int length, bool clear)
+    {
+        Length = length;
+        _dataI = ArrayPool<T>.Shared.Rent(length);
+        _dataQ = ArrayPool<T>.Shared.Rent(length);
+        if (clear)
+        {
+            ClearData();
+        }
+    }
+
+    public PooledComplexArray(ComplexArrayReadOnlySpan<T> source) : this(source.Length, false)
+    {
+        source.DataI.CopyTo(_dataI);
+        source.DataQ.CopyTo(_dataQ);
+    }
+
+    public PooledComplexArray<T> Copy()
+    {
+        return new PooledComplexArray<T>(this);
+    }
+
+    public void Dispose()
+    {
+        if (_disposed)
+        {
+            return;
+        }
+
+        _disposed = true;
+        ArrayPool<T>.Shared.Return(_dataI, false);
+        ArrayPool<T>.Shared.Return(_dataQ, false);
+    }
+
+    private void ClearData()
+    {
+        _dataI.AsSpan(0, Length).Clear();
+        _dataQ.AsSpan(0, Length).Clear();
+    }
+}
