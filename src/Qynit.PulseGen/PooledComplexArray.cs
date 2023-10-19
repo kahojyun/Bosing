fe438a2085e922a258879fa98baa7363e36315db
Jiahao Yuan <kaho0769@qq.com>
2023-10-19 21:01:06 +0800

    try using NativeMemory api

--- src/Qynit.PulseGen/PooledComplexArray.cs
+++ src/Qynit.PulseGen/PooledComplexArray.cs
@@ -1,5 +1,6 @@
 using System.Buffers;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
 using CommunityToolkit.Diagnostics;
 
@@ -7,44 +8,47 @@ namespace Qynit.PulseGen;
 
 public abstract class PooledComplexArray { }
 
-public sealed class PooledComplexArray<T> : PooledComplexArray, IDisposable
+public unsafe sealed class PooledComplexArray<T> : PooledComplexArray, IDisposable
     where T : unmanaged
 {
     public int Length { get; }
     public bool IsEmpty => Length == 0;
     public bool IsReal { get; set; }
-    private static readonly ArrayPool<T> ArrayPool = ArrayPool<T>.Create(1000000, 1000);
     public Span<T> DataI
     {
         get
         {
-            if (_disposed)
+            if (_dataI is null)
             {
                 ThrowHelper.ThrowObjectDisposedException(nameof(PooledComplexArray<T>));
             }
-            return _dataI.AsSpan(0, Length);
+            return new Span<T>(_dataI, Length);
         }
     }
     public Span<T> DataQ
     {
         get
         {
-            if (_disposed)
+            if (_dataQ is null)
             {
                 ThrowHelper.ThrowObjectDisposedException(nameof(PooledComplexArray<T>));
             }
-            return _dataQ.AsSpan(0, Length);
+            return new Span<T>(_dataQ, Length);
         }
     }
-    private readonly T[] _dataI;
-    private readonly T[] _dataQ;
-    private bool _disposed;
+    private void* _dataI;
+    private void* _dataQ;
+    private bool _disposedValue;
 
     public PooledComplexArray(int length, bool clear)
     {
+        if (length < 0)
+        {
+            ThrowHelper.ThrowArgumentOutOfRangeException(nameof(length));
+        }
         Length = length;
-        _dataI = ArrayPool.Rent(length);
-        _dataQ = ArrayPool.Rent(length);
+        _dataI = NativeMemory.AlignedAlloc((nuint)length * (nuint)Unsafe.SizeOf<T>(), 256 / 8);
+        _dataQ = NativeMemory.AlignedAlloc((nuint)length * (nuint)Unsafe.SizeOf<T>(), 256 / 8);
         if (clear)
         {
             Clear();
@@ -52,8 +56,8 @@ public sealed class PooledComplexArray<T> : PooledComplexArray, IDisposable
     }
     public PooledComplexArray(ComplexReadOnlySpan<T> source) : this(source.Length, false)
     {
-        source.DataI.CopyTo(_dataI);
-        source.DataQ.CopyTo(_dataQ);
+        source.DataI.CopyTo(DataI);
+        source.DataQ.CopyTo(DataQ);
     }
 
     public PooledComplexArray<T> Copy()
@@ -79,14 +83,43 @@ public sealed class PooledComplexArray<T> : PooledComplexArray, IDisposable
         DataI.Clear();
         DataQ.Clear();
     }
+
+    private void Dispose(bool disposing)
+    {
+        if (!_disposedValue)
+        {
+            if (disposing)
+            {
+                // TODO: dispose managed state (managed objects)
+            }
+
+            // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+            // TODO: set large fields to null
+            _disposedValue = true;
+            if (_dataI is not null)
+            {
+                NativeMemory.AlignedFree(_dataI);
+                _dataI = null;
+            }
+            if (_dataQ is not null)
+            {
+                NativeMemory.AlignedFree(_dataQ);
+                _dataQ = null;
+            }
+        }
+    }
+
+    // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+    ~PooledComplexArray()
+    {
+        // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        Dispose(disposing: false);
+    }
+
     public void Dispose()
     {
-        if (_disposed)
-        {
-            return;
-        }
-        _disposed = true;
-        ArrayPool.Return(_dataI, false);
-        ArrayPool.Return(_dataQ, false);
+        // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        Dispose(disposing: true);
+        GC.SuppressFinalize(this);
     }
 }
