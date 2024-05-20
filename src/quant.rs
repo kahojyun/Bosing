dc561cdc47b69811fe0f53ffc0d2a25f1d26dada
kaho <kaho0769@qq.com>
2024-05-20 18:36:05 +0800

    fix: better error handling

--- src/quant.rs
+++ src/quant.rs
@@ -4,11 +4,119 @@ use std::{
     sync::Arc,
 };
 
-use anyhow::{bail, Result};
 use num::NumCast;
 use numpy::Complex64;
 use ordered_float::NotNan;
-use pyo3::{prelude::*, types::PyFloat, IntoPy};
+use pyo3::{exceptions::PyValueError, prelude::*, types::PyFloat, IntoPy};
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub(crate) enum Error {
+    #[error("NaN value is not allowed")]
+    NanValue(#[from] ordered_float::FloatIsNan),
+    #[error("Infinite value is not allowed")]
+    InfiniteValue,
+}
+
+macro_rules! def_quant {
+    ($t:ident) => {
+        #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord, Default)]
+        pub(crate) struct $t(NotNan<f64>);
+    };
+}
+
+def_quant!(Time);
+def_quant!(Frequency);
+def_quant!(Phase);
+def_quant!(Amplitude);
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord)]
+pub(crate) struct AlignedIndex(NotNan<f64>);
+
+macro_rules! def_id {
+    ($t:ident) => {
+        #[derive(Debug, Clone, PartialEq, Eq, Hash)]
+        pub(crate) struct $t(Arc<str>);
+    };
+}
+
+def_id!(ChannelId);
+def_id!(ShapeId);
+
+type Result<T> = std::result::Result<T, Error>;
+
+impl Time {
+    pub(crate) const INFINITY: Self = Self(unsafe { NotNan::new_unchecked(f64::INFINITY) });
+}
+
+impl Phase {
+    fn radians(&self) -> f64 {
+        self.value() * std::f64::consts::TAU
+    }
+
+    pub(crate) fn phaser(&self) -> Complex64 {
+        Complex64::from_polar(1.0, self.radians())
+    }
+}
+
+impl Frequency {
+    pub(crate) fn dt(&self) -> Time {
+        Time::new(1.0 / self.value()).expect("Frequency should be non-zero")
+    }
+}
+
+impl AlignedIndex {
+    pub(crate) fn new(time: Time, sample_rate: Frequency, align_level: i32) -> Result<Self> {
+        fn scaleb(x: f64, s: i32) -> f64 {
+            x * (s as f64).exp2()
+        }
+        let scaled_sr = scaleb(sample_rate.value(), -align_level);
+        let i = (time.value() * scaled_sr).ceil();
+        let aligned_index = scaleb(i, align_level);
+        Self::from_value(aligned_index)
+    }
+
+    fn from_value(value: f64) -> Result<Self> {
+        if value.is_infinite() {
+            return Err(Error::InfiniteValue);
+        }
+        Ok(Self(NotNan::new(value)?))
+    }
+
+    pub(crate) fn value(&self) -> f64 {
+        self.0.into_inner()
+    }
+
+    pub(crate) fn ceil_to_usize(&self) -> Option<usize> {
+        <usize as NumCast>::from(self.0.ceil())
+    }
+
+    pub(crate) fn index_offset(&self) -> Result<Self> {
+        Self::from_value(self.0.ceil() - self.0.into_inner())
+    }
+}
+
+impl Mul<Time> for Frequency {
+    type Output = Phase;
+
+    fn mul(self, rhs: Time) -> Self::Output {
+        Phase::new(self.value() * rhs.value()).expect("Should be a valid phase value")
+    }
+}
+
+impl Mul<Frequency> for Time {
+    type Output = Phase;
+
+    fn mul(self, rhs: Frequency) -> Self::Output {
+        Phase::new(self.value() * rhs.value()).expect("Should be a valid phase value")
+    }
+}
+
+impl From<Error> for PyErr {
+    fn from(err: Error) -> Self {
+        PyValueError::new_err(err.to_string())
+    }
+}
 
 macro_rules! forward_ref_binop {
     ($trait:ident, $method:ident, $t:ty) => {
@@ -39,10 +147,7 @@ macro_rules! forward_ref_binop {
 }
 
 macro_rules! impl_quant {
-    ($t:ident) => {
-        #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord, Default)]
-        pub(crate) struct $t(NotNan<f64>);
-
+    ($t:ty) => {
         impl $t {
             pub(crate) fn new(value: f64) -> Result<Self> {
                 Ok(Self(NotNan::new(value)?))
@@ -151,7 +256,7 @@ macro_rules! impl_quant {
         }
 
         impl TryFrom<f64> for $t {
-            type Error = anyhow::Error;
+            type Error = Error;
 
             fn try_from(value: f64) -> Result<Self> {
                 Self::new(value)
@@ -165,84 +270,17 @@ impl_quant!(Frequency);
 impl_quant!(Phase);
 impl_quant!(Amplitude);
 
-impl Time {
-    pub(crate) const INFINITY: Self = Self(unsafe { NotNan::new_unchecked(f64::INFINITY) });
-}
-
-impl Phase {
-    fn radians(&self) -> f64 {
-        self.value() * std::f64::consts::TAU
-    }
-
-    pub(crate) fn phaser(&self) -> Complex64 {
-        Complex64::from_polar(1.0, self.radians())
-    }
-}
-
-impl Frequency {
-    pub(crate) fn dt(&self) -> Time {
-        Time::new(1.0 / self.value()).expect("Frequency should be non-zero")
-    }
-}
-
-impl Mul<Time> for Frequency {
-    type Output = Phase;
-
-    fn mul(self, rhs: Time) -> Self::Output {
-        Phase::new(self.value() * rhs.value()).expect("Should be a valid phase value")
-    }
-}
-
-impl Mul<Frequency> for Time {
-    type Output = Phase;
-
-    fn mul(self, rhs: Frequency) -> Self::Output {
-        Phase::new(self.value() * rhs.value()).expect("Should be a valid phase value")
-    }
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord)]
-pub(crate) struct AlignedIndex(NotNan<f64>);
-
-impl AlignedIndex {
-    pub(crate) fn new(time: Time, sample_rate: Frequency, align_level: i32) -> Result<Self> {
-        fn scaleb(x: f64, s: i32) -> f64 {
-            x * (s as f64).exp2()
-        }
-        let scaled_sr = scaleb(sample_rate.value(), -align_level);
-        let i = (time.value() * scaled_sr).ceil();
-        let aligned_index = scaleb(i, align_level);
-        Self::from_value(aligned_index)
-    }
-
-    fn from_value(value: f64) -> Result<Self> {
-        if value.is_infinite() {
-            bail!("Infinite value is not allowed");
-        }
-        Ok(Self(NotNan::new(value)?))
-    }
-
-    pub(crate) fn value(&self) -> f64 {
-        self.0.into_inner()
-    }
-
-    pub(crate) fn ceil_as_usize(&self) -> Option<usize> {
-        <usize as NumCast>::from(self.0.ceil())
-    }
-
-    pub(crate) fn index_offset(&self) -> Result<Self> {
-        Self::from_value(self.0.ceil() - self.0.into_inner())
-    }
-}
-
 macro_rules! impl_id {
-    ($t:ident) => {
-        #[derive(Debug, Clone, PartialEq, Eq, Hash)]
-        pub(crate) struct $t(Arc<str>);
-
+    ($t:ty) => {
         impl $t {
             pub(crate) fn new(name: impl Into<Arc<str>>) -> Self {
                 Self(name.into())
+            }
+        }
+
+        impl std::fmt::Display for $t {
+            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                write!(f, "{}", self.0)
             }
         }
 
