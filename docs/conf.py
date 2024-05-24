4449a89b496eb5d44582f1b9c17ca47cf3ba5151
kaho <kaho0769@qq.com>
2024-05-24 17:03:52 +0800

    chore: upgrade hatch & ci lint & docs requirement

--- docs/conf.py
+++ docs/conf.py
@@ -7,7 +7,7 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = "bosing"
-copyright = "2023, Jiahao Yuan"
+copyright = "2023, Jiahao Yuan"  # noqa: A001
 author = "Jiahao Yuan"
 
 # -- General configuration ---------------------------------------------------
