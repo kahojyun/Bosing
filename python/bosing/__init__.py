545b01449b9716c60cf73017f586107efeb0322c
Jiahao Yuan <kaho0769@qq.com>
2024-10-21 17:36:10 +0800

    fix ruff lint

--- python/bosing/__init__.py
+++ python/bosing/__init__.py
@@ -6,5 +6,5 @@ r"""Generates microwave pulses for superconducting quantum computing experiments
     of :math:`0.5` means a phase shift of :math:`\pi` radians.
 """
 
-from ._bosing import *
+from ._bosing import *  # noqa: F403
 from ._bosing import __all__ as __all__
