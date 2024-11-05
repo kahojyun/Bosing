df458dcb6fcd3ac6504e14038c9a556e4671a631
Jiahao Yuan <kaho0769@qq.com>
2024-11-05 18:43:32 +0800

    use declarative pymodule definition

--- src/python.rs
+++ src/python.rs
@@ -1,48 +1,26 @@
+//! Python bindings for the bosing library.
 mod extract;
 mod plot;
 mod repr;
 
-use plot::ItemKind;
 use pyo3::prelude::*;
 
-use elements::{
-    Absolute, AbsoluteEntry, Barrier, Element, Grid, GridEntry, Play, Repeat, SetFreq, SetPhase,
-    ShiftFreq, ShiftPhase, Stack, SwapPhase,
-};
-use shapes::{Hann, Interp, Shape};
-use wavegen::{generate_waveforms, generate_waveforms_with_states, Channel, OscState};
+pub(crate) use elements::{Alignment, Direction, GridLength};
 
-pub(crate) use elements::{Alignment, Direction, GridLength, GridLengthUnit};
-
-#[pymodule]
-fn _bosing(m: &Bound<PyModule>) -> PyResult<()> {
-    m.add_class::<Absolute>()?;
-    m.add_class::<AbsoluteEntry>()?;
-    m.add_class::<Alignment>()?;
-    m.add_class::<Barrier>()?;
-    m.add_class::<Channel>()?;
-    m.add_class::<Direction>()?;
-    m.add_class::<Element>()?;
-    m.add_class::<Grid>()?;
-    m.add_class::<GridEntry>()?;
-    m.add_class::<GridLength>()?;
-    m.add_class::<GridLengthUnit>()?;
-    m.add_class::<Hann>()?;
-    m.add_class::<Interp>()?;
-    m.add_class::<Play>()?;
-    m.add_class::<Repeat>()?;
-    m.add_class::<SetFreq>()?;
-    m.add_class::<SetPhase>()?;
-    m.add_class::<ShiftFreq>()?;
-    m.add_class::<ShiftPhase>()?;
-    m.add_class::<Shape>()?;
-    m.add_class::<Stack>()?;
-    m.add_class::<SwapPhase>()?;
-    m.add_class::<OscState>()?;
-    m.add_function(wrap_pyfunction!(generate_waveforms, m)?)?;
-    m.add_function(wrap_pyfunction!(generate_waveforms_with_states, m)?)?;
-    m.add_class::<ItemKind>()?;
-    Ok(())
+/// Export the bosing library to Python.
+#[pymodule(name = "_bosing")]
+mod export {
+    #[pymodule_export]
+    use super::{
+        elements::{
+            Absolute, AbsoluteEntry, Alignment, Barrier, Direction, Element, Grid, GridEntry,
+            GridLength, GridLengthUnit, Play, Repeat, SetFreq, SetPhase, ShiftFreq, ShiftPhase,
+            Stack, SwapPhase,
+        },
+        plot::ItemKind,
+        shapes::{Hann, Interp, Shape},
+        wavegen::{generate_waveforms, generate_waveforms_with_states, Channel, OscState},
+    };
 }
 
 macro_rules! push_repr {
