a0617c75c527f47d7d4a50834016f011a8e02b58
Jiahao Yuan <kahojyun@icloud.com>
2024-11-28 05:02:18 +0800

    update maturin manifest path

--- bosing-py/src/lib.rs
+++ bosing-py/src/lib.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use pyo3::prelude::*;
+
+#[pymodule]
+pub mod _bosing {
+    use pyo3::prelude::*;
+
+    #[must_use]
+    #[pyfunction]
+    pub const fn add(a: usize, b: usize) -> usize {
+        a + b
+    }
 }
